fab9842389c56494616240345c0bc53506a00e86
Mk-stark <87647150+Mk-stark@users.noreply.github.com>
2022-05-15 19:54:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,11 @@ class Bot(Client):
         super().__init__(name=CHAT_ID, session_string=SESSION)
     async def start(self):
         await super().start()
-        while True:
+        def send():
             m = await self.send_message(chat_id=CHAT_ID, text=TEXT)
             await m.delete()
+        while True:
+            for i in range(5):
+                send()
             await sleep(SLEEP)
 Bot().run()
